f8163af56a2105ea1597ba3d59bde67c937d9ef8
jchene <jchene@student.42.fr>
2021-11-18 19:08:11 +0100

    julien

--- headers/minishell.h
+++ headers/minishell.h
@@ -6,13 +6,12 @@
 /*   By: jchene <jchene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 17:49:28 by hlevi             #+#    #+#             */
-/*   Updated: 2021/11/17 15:41:25 by jchene           ###   ########.fr       */
+/*   Updated: 2021/11/18 19:07:01 by jchene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
-# define MAX_PATH 200
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
@@ -23,6 +22,12 @@
 # include <sys/wait.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
+//var defines
+# define MAX_PATH 200
+
+//error defines
+# define RL_EOF 1
 
 typedef struct s_msh
 {
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,16 +6,33 @@
 /*   By: jchene <jchene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 17:53:27 by hlevi             #+#    #+#             */
-/*   Updated: 2021/11/17 15:55:41 by jchene           ###   ########.fr       */
+/*   Updated: 2021/11/18 19:07:36 by jchene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
 
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	err_handler(int err_code)
+{
+	if (err_code == RL_EOF)
+		printf("bad_path\n");
+	return (-1);
+}
+
 int	main(int argc, char **argv, char **env)
 {
-
-//test
 	t_msh	mini;
 
 	(void)env;
@@ -23,9 +40,11 @@ int	main(int argc, char **argv, char **env)
 	(void)argv;
 	while (1)
 	{
-		mini.line = readline("minishell:PATH$\t");
+		mini.line = readline("$> ");
+		if (!mini.line)
+			break ;
 		printf("%s\n", mini.line);
-		free(mini.line);	
+		free(mini.line);
 	}
 	return (0);
 }
