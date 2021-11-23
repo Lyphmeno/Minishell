6baeb7a8368d740b61f53a55656e983c10fa8801
Lyphmeno <hugolevipro@gmail.com>
2021-11-23 15:06:46 +0100

    updatye

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,18 +6,23 @@
 /*   By: hlevi <hlevi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 16:30:35 by hlevi             #+#    #+#             */
-/*   Updated: 2021/11/18 17:09:59 by hlevi            ###   ########.fr       */
+/*   Updated: 2021/11/23 14:23:14 by hlevi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
+
+void	split_quotes(t_msh *mini)
+{
+	mini->linetab = ft_split(mini->line, ' ');
+}
 
 void	parsing_base(t_msh *mini)
 {
 	int	i;
 
 	i = 0;
-	mini->linetab = ft_split(mini->line, ' ');
+	split_quotes(mini);
 	while (mini->linetab[i] != NULL)
 	{
 		printf("%s\n", mini->linetab[i]);
