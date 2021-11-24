78249412fa60deb5d33690f95afded61ccfc4ecb
jchene <jchene@student.42.fr>
2021-11-24 13:08:11 +0100

    pasring

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: jchene <jchene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 16:30:35 by hlevi             #+#    #+#             */
-/*   Updated: 2021/11/23 16:10:18 by jchene           ###   ########.fr       */
+/*   Updated: 2021/11/24 13:04:38 by jchene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ void	parsing_base(void)
 	int	i;
 
 	i = 0;
-	s_mini()->linetab = ft_split(s_mini()->line, ' ');
-	while (s_mini()->linetab[i] != NULL)
+	mini()->linetab = ft_split(mini()->line, ' ');
+	while (mini()->linetab[i] != NULL)
 	{
-		printf("%s\n", s_mini()->linetab[i]);
+		printf("%s\n", mini()->linetab[i]);
 		i++;
 	}
 }
