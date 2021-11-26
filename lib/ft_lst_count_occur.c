1fa79dc907bc66bfdd8c04661bcd3c1e3c2026b1
jchene <jchene@student.42.fr>
2021-11-26 22:02:13 +0100

    .

--- lib/ft_lst_count_occur.c
+++ lib/ft_lst_count_occur.c
@@ -6,12 +6,13 @@
 /*   By: jchene <jchene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 19:10:14 by jchene            #+#    #+#             */
-/*   Updated: 2021/11/24 19:12:25 by jchene           ###   ########.fr       */
+/*   Updated: 2021/11/26 21:33:43 by jchene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
 
+//Count occurences of char c in every char * of list lst
 int	ft_lst_count_occur(t_start *lst, char c)
 {
 	int		quotes_nb;
--- lib/ft_lst_join.c
+++ lib/ft_lst_join.c
@@ -6,7 +6,7 @@
 /*   By: jchene <jchene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 16:40:52 by jchene            #+#    #+#             */
-/*   Updated: 2021/11/26 03:42:43 by jchene           ###   ########.fr       */
+/*   Updated: 2021/11/26 22:02:08 by jchene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,16 @@ int	ft_lst_join(void)
 	t_elem	*close_word;
 	t_elem	*tmp;
 
-	open_word = NULL;
 	close_word = NULL;
 	tmp = st_words()->first;
 	if (!tmp)
 		return (0);
 	if (((ft_lst_count_occur(st_words(), '"')) % 2) != 0)
 		return (-1);
+	while (tmp->next)
+	{
+		open_word = ft_lst_next_occur(tmp)
+	}
 	//Travail en cours ici
 	/*if (!open_word)
 		open_word = tmp;
