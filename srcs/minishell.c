94838521703ee366e097c27c721a0aca08c94a61
Lyphmeno <hugolevipro@gmail.com>
2022-01-10 15:18:45 +0100

    env command done and printing env done

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: hlevi <hlevi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 17:53:27 by hlevi             #+#    #+#             */
-/*   Updated: 2022/01/10 15:01:10 by hlevi            ###   ########.fr       */
+/*   Updated: 2022/01/10 15:17:57 by hlevi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,17 @@ void	print_env(t_env *env)
 	t_env	*tmp;
 
 	i = 0;
-	printf("--------------------VARIABLE ENV--------------------\n");
 	tmp = env;
 	if (tmp != NULL)
 	{
 		while (tmp->next != NULL)
 		{
-			printf("[%s]:----> %s\n", tmp->key, tmp->value);
+			printf("%s=%s\n", tmp->key, tmp->value);
 			tmp = tmp->next;
 			i++;
 		}
-		printf("v[%s]:----> NULL\n", tmp->key);
+		printf("%s=%s\n", tmp->key, tmp->value);
 	}
-	printf("----------------------------------------\n\n");
 }
 
 /*void	sighandler(int sig)
@@ -48,11 +46,11 @@ int	main(int argc, char **argv, char **envp)
 	env = NULL;
 	env = parse_env(envp);
 	print_env(env);
-	// while (1)
-	// {
-	// 	st_mini()->line = readline("$> ");
-	// 	//parsing_base();
-	// 	free(st_mini()->line);
-	// }
+	while (1)
+	{
+		st_mini()->line = readline("$> ");
+		//parsing_base();
+		free(st_mini()->line);
+	}
 	return (0);
 }
